75542cf7819e48b49b1fc81ed5420c5606ff6456
Robin Leach <Robin.Leach@softwire.com>
2018-07-12 15:45:59 +0100

    Started building the app by setting up class for the bus details

---
+++ BusBoard.ConsoleApp/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusBoard.ConsoleApp
+{
+    class BusData
+    {
+        private string stationName, lineId, direction, destinationName;
+        private DateTime timestamp;
+        private double timeToStation;
+
+        public BusData (string stationName, string lineId, string direction, string destinationName, DateTime timestamp, double timeToStation)
+        {
+            this.stationName = stationName;
+            this.lineId = lineId;
+            this.direction = direction;
+            this.destinationName = destinationName;
+            this.timestamp = timestamp;
+            this.timeToStation = timeToStation;
+
+        }
+
+    }
+}
--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
 
 namespace BusBoard.ConsoleApp
 {
@@ -10,6 +13,22 @@ namespace BusBoard.ConsoleApp
   {
     static void Main(string[] args)
     {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+
+            var client = new RestClient();
+            client.BaseUrl = new Uri("https://api.tfl.gov.uk");
+            client.Authenticator = new SimpleAuthenticator("app_id", "48fc64da", "app_key", "13b3e4935f4929c443b2391efe204741");
+
+            var request = new RestRequest();
+            request.Resource = "StopPoint/490008660N/Arrivals";
+            IRestResponse response = client.Execute(request);
+
+            string data = response.Content;
+
+        
+
+
+
+        }
     }
-  }
 }

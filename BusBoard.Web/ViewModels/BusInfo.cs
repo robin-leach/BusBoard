7b2f307156cb627dea87c53dab841e8a68d5b36e
Robin Leach <Robin.Leach@softwire.com>
2018-07-13 14:49:20 +0100

    adds in an error page

--- BusBoard.Web/ViewModels/BusInfo.cs
+++ BusBoard.Web/ViewModels/BusInfo.cs
@@ -9,33 +9,28 @@ namespace BusBoard.Web.ViewModels
         public BusInfo(string postCode)
         {
             PostCode = postCode;
-            try
+
+            string[] Location = API.GetLocation(PostCode);
+            string latitude = Location[0];
+            string longitude = Location[1];
+            List<BusStop> nearest_stops = API.GetNearestStop(latitude, longitude);
+            output_info = new List<string>();
+            Data = new List<List<BusData>>();
+            printable_data = new List<string>();
+            foreach (var stop in nearest_stops)
             {
-                string[] Location = API.GetLocation(PostCode);
-                string latitude = Location[0];
-                string longitude = Location[1];
-                List<BusStop> nearest_stops = API.GetNearestStop(latitude, longitude);
-                output_info = new List<string>();
-                Data = new List<List<BusData>>();
-                printable_data = new List<string>();
-                foreach (var stop in nearest_stops)
+                List<BusData> BusList = API.GetData(stop.stopId);
+                string bus_info_message = BusList[0].StationName + " is " + Math.Round(stop.distance, 0) + "m away";
+                output_info.Add(bus_info_message);
+                BusList = API.SortByTime(BusList);
+                Data.Add(BusList);
+                foreach (var bus in BusList)
                 {
-                    List<BusData> BusList = API.GetData(stop.stopId);
-                    string bus_info_message = BusList[0].StationName + " is " + Math.Round(stop.distance, 0) + "m away";
-                    output_info.Add(bus_info_message);
-                    BusList = API.SortByTime(BusList);
-                    Data.Add(BusList);
-                    foreach (var bus in BusList)
-                    {
-                        string bus_info_data = bus.DataToString();
-                        printable_data.Add(bus_info_data);
-                    }
+                    string bus_info_data = bus.DataToString();
+                    printable_data.Add(bus_info_data);
                 }
             }
-            catch
-            {
-                PostCode = "error";
-            }
+
 
         }
 

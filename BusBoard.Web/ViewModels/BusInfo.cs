3f25d01c8214b246765f137ddb51c0e5c460f442
Robin Leach <Robin.Leach@softwire.com>
2018-07-13 14:33:56 +0100

    starts work on the error page with a try/catch

--- BusBoard.Web/ViewModels/BusInfo.cs
+++ BusBoard.Web/ViewModels/BusInfo.cs
@@ -9,26 +9,34 @@ namespace BusBoard.Web.ViewModels
         public BusInfo(string postCode)
         {
             PostCode = postCode;
-            string[] Location = API.GetLocation(PostCode);
-            string latitude = Location[0];
-            string longitude = Location[1];
-            List<BusStop> nearest_stops = API.GetNearestStop(latitude, longitude);
-            output_info = new List<string>();
-            Data = new List<List<BusData>>();
-            printable_data = new List<string>();
-            foreach (var stop in nearest_stops)
+            try
             {
-                List<BusData> BusList = API.GetData(stop.stopId);
-                string bus_info_message = BusList[0].StationName + " is " + Math.Round(stop.distance, 2) + "m away";
-                output_info.Add(bus_info_message);
-                BusList = API.SortByTime(BusList);
-                Data.Add(BusList);
-                foreach(var bus in BusList)
+                string[] Location = API.GetLocation(PostCode);
+                string latitude = Location[0];
+                string longitude = Location[1];
+                List<BusStop> nearest_stops = API.GetNearestStop(latitude, longitude);
+                output_info = new List<string>();
+                Data = new List<List<BusData>>();
+                printable_data = new List<string>();
+                foreach (var stop in nearest_stops)
                 {
-                    string bus_info_data = bus.DataToString();
-                    printable_data.Add(bus_info_data);
+                    List<BusData> BusList = API.GetData(stop.stopId);
+                    string bus_info_message = BusList[0].StationName + " is " + Math.Round(stop.distance, 0) + "m away";
+                    output_info.Add(bus_info_message);
+                    BusList = API.SortByTime(BusList);
+                    Data.Add(BusList);
+                    foreach (var bus in BusList)
+                    {
+                        string bus_info_data = bus.DataToString();
+                        printable_data.Add(bus_info_data);
+                    }
                 }
             }
+            catch
+            {
+                PostCode = "error";
+            }
+
         }
 
         public string PostCode { get; set; }
